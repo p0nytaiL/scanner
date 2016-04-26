57e9417aa88d945067735e816bda7a359c8103c5
ponytail <ponytail@Ponytail.local>
2016-04-26 21:26:43 +0800

    commit
    ports

---
+++ PackageScanner/Functions/Host/MailServer/Command.py
@@ -0,0 +1,7 @@
+'''
+
+SMTP Command
+
+HELO是普通SMTP，不带身份验证也可以继续MAIL FROM。。。下去，直到成功发送邮件，也就是可以伪造邮件啦！
+EHLO是ESMTP，带有身份验证，所以没法伪造。
+'''
---
+++ PackageScanner/Functions/Host/MailServer/Servers/zimbra.py
@@ -0,0 +1,13 @@
+'''
+zimbra
+Version 6.0 7.0. 8.0
+任意文件读取
+
+默认日志路径
+/var/log/zimbra.log
+/opt/zimbra/jetty/logs/xxxx_xx_xx.trace.log%00
+
+读session
+添加cookie
+bypass
+'''
